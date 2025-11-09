5aca260e5dd09defb2c7fcc5147fed583e642256
Lakshay Sharma <lakshayiitian12@gmail.com>
2025-11-09 23:57:55 +0530

    Add VariableNames class with various data types

---
+++ variable.java
@@ -0,0 +1,22 @@
+public class VariableNames {
+    public static void main(String[] args) {
+        int x;
+        x = 5;
+    System.out.println(x);
+
+        float d;
+        d = 23.8f;   // Add 'f' to indicate it's a float literal
+        System.out.println(d);
+
+        char c;
+        c = 'L';   // Only a single character is allowed in single quotes
+        System.out.println(c);
+        short s;
+        s=98;
+        System.out.println(s);
+        byte b;
+        b=-87;
+        System.out.println(b);
+        
+    }
+}
